cc9686a4dd5b526c8bf81317bce1d7807e2da245
rohit sah <k97rohit@gmail.com>
2026-01-08 18:09:10 +0530

    Initial commit

---
+++ consumer2.js
@@ -0,0 +1,36 @@
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092']
+})
+
+// const producer = kafka.producer()
+const consumer = kafka.consumer({ groupId: 'test-group2' })
+
+const run = async () => {
+  // Producing
+//   await producer.connect()
+//   await producer.send({
+//     topic: 'quickstart-events',
+//     messages: [
+//       { value: 'Hello KafkaJS user!' },
+//     ],
+//   })
+
+  // Consuming
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'greet', fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      })
+    },
+  })
+}
+
+run().catch(console.error)
